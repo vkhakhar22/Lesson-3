80dcf315b866f972cd44c92aef6513797c263c45
vkhakhar22 <vanshikhakhar22.svkm@gmail.com>
2024-10-27 13:19:36 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,13 @@
+public class Loop {
+    public static void main(String[]args){
+        for(int i=10;i>1;i--){
+            System.out.println("The value of i is: "+i);
+
+        }
+        int j=0;
+        while (j<11){
+            System.out.println("The value of j is: "+j);
+            j++;
+        }
+    }
+}
